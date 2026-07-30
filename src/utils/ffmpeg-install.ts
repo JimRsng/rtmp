1d9f4d4cad49ae31c0936137ad67e74bceb68af0
Ahmed Rangel <ahmedrangel@outlook.com>
2026-07-30 11:31:45 -0500

    chore: replace ofetch with $fetch

--- src/utils/ffmpeg-install.ts
+++ src/utils/ffmpeg-install.ts
@@ -1,4 +1,4 @@
-import { ofetch } from "ofetch";
+import { $fetch } from "ofetch";
 import { pipeline } from "node:stream/promises";
 import { createReadStream, createWriteStream } from "node:fs";
 import { unlink } from "node:fs/promises";
@@ -8,7 +8,7 @@ import { getDirs } from "./helpers.ts";
 export async function installFFmpeg ({ target }: { target: string }) {
   const { runtimeDir } = await getDirs();
   const url = `https://github.com/eugeneware/ffmpeg-static/releases/download/b6.1.1/${target}.gz`;
-  const response = await ofetch(url, { responseType: "stream" });
+  const response = await $fetch(url, { responseType: "stream" });
   const gzFile = `${runtimeDir}/ffmpeg.gz`;
   await pipeline(response, createWriteStream(gzFile));
   await pipeline(
